b756c764859f616c4431ba1e1e94c0946ece2466
bhechinger <wonko@4amlunch.net>
2022-01-06 13:32:22 +0000

    oops, forgot to close the triple ticks

--- internal/commands/version.go
+++ internal/commands/version.go
@@ -14,7 +14,7 @@ var version string
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func (c Command) Version(s *discordgo.Session, m *discordgo.Message, ctx *mux.Context) {
-	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("```Version: %s", version))
+	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("```Version: %s```", version))
 	if err != nil {
 		c.dependencies.Logger.Errorf("Error sending command: %s", err)
 	}
