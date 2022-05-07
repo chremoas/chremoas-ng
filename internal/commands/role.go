5672360a8475eca149c33b9e49ce7458de04c0f0
bhechinger <wonko@4amlunch.net>
2022-05-07 21:09:32 +0100

    nope, not doing roles. that's too much

--- internal/commands/role.go
+++ internal/commands/role.go
@@ -60,12 +60,14 @@ func (c Command) doRole(ctx context.Context, m *discordgo.Message) []*discordgo.
 	switch cmdStr[1] {
 	case "list":
 		if len(cmdStr) < 3 {
-			return roles.List(ctx, roles.Role, false, m.ChannelID, c.dependencies)
+			return common.SendError("Just.... no")
+			// return roles.List(ctx, roles.Role, false, m.ChannelID, c.dependencies)
 		}
 
 		switch cmdStr[2] {
 		case "all":
-			return roles.List(ctx, roles.Role, true, m.ChannelID, c.dependencies)
+			return common.SendError("Just.... no")
+			// return roles.List(ctx, roles.Role, true, m.ChannelID, c.dependencies)
 
 		case "members":
 			if len(cmdStr) < 4 {
